f959e31cf4cd72f367eb973e1d861e175ab97bc4
Hoang Nguyen <hoangnguyen3892@users.noreply.github.com>
2017-11-20 00:00:27 +0900

    Update data_preprocessing.sh

--- data_preprocessing/data_preprocessing.sh
+++ data_preprocessing/data_preprocessing.sh
@@ -5,7 +5,7 @@ pydata
 #python induce_missing_data.py -n bpi_2013 --nan_pct 0.4
 #python induce_missing_data.py -n bpi_2013 --nan_pct 0.5
 
-#python induce_missing_data.py -n bpi_2012 --nan_pct 0.35
+python induce_missing_data.py -n bpi_2012 --nan_pct 0.3
 #python induce_missing_data.py -n bpi_2012 --nan_pct 0.35
 #python induce_missing_data.py -n bpi_2012 --nan_pct 0.4
 #python induce_missing_data.py -n bpi_2012 --nan_pct 0.5
@@ -16,21 +16,21 @@ pydata
 #python preprocess_variables.py -n bpi_2013 --nan_pct 0.4
 #python preprocess_variables.py -n bpi_2013 --nan_pct 0.5
 
-#python preprocess_variables.py -n bpi_2012 --nan_pct 0.3
+python preprocess_variables.py -n bpi_2012 --nan_pct 0.3
 #python preprocess_variables.py -n bpi_2012 --nan_pct 0.35
 #python preprocess_variables.py -n bpi_2012 --nan_pct 0.4
-python preprocess_variables.py -n bpi_2012 --nan_pct 0.5
+#python preprocess_variables.py -n bpi_2012 --nan_pct 0.5
 
 
-python preprocess_variables_full.py -n bpi_2013 --nan_pct 0.3
-python preprocess_variables_full.py -n bpi_2013 --nan_pct 0.35
-python preprocess_variables_full.py -n bpi_2013 --nan_pct 0.4
-python preprocess_variables_full.py -n bpi_2013 --nan_pct 0.5
+#python preprocess_variables_full.py -n bpi_2013 --nan_pct 0.3
+#python preprocess_variables_full.py -n bpi_2013 --nan_pct 0.35
+#python preprocess_variables_full.py -n bpi_2013 --nan_pct 0.4
+#python preprocess_variables_full.py -n bpi_2013 --nan_pct 0.5
 
 python preprocess_variables_full.py -n bpi_2012 --nan_pct 0.3
-python preprocess_variables_full.py -n bpi_2012 --nan_pct 0.35
-python preprocess_variables_full.py -n bpi_2012 --nan_pct 0.4
-python preprocess_variables_full.py -n bpi_2012 --nan_pct 0.5
+#python preprocess_variables_full.py -n bpi_2012 --nan_pct 0.35
+#python preprocess_variables_full.py -n bpi_2012 --nan_pct 0.4
+#python preprocess_variables_full.py -n bpi_2012 --nan_pct 0.5
 
 
 pyexit
